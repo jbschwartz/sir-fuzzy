cf733ba4b77a786d07b781f3775327b5ac432985
James Schwartz <james@schwartz.engineer>
2015-08-05 20:07:19 -0400

    Initial commit. First pass at fuzzy search.

---
+++ sir-fuzzy.js
@@ -0,0 +1,44 @@
+var match = function(needle, haystack) {
+    var lastWhitespace = 0;
+    var result = [];
+    haystack.split("").forEach(function(hayChar, hayCharIndex) {
+        needle.split("").some(function(needleChar, needleCharIndex) {
+            if(!result[needleCharIndex]) { result[needleCharIndex] = [] }
+
+            if(hayChar == ' ') {
+                lastWhitespace = hayCharIndex;
+                return true;
+            }
+
+            if(needleChar.toLowerCase() == hayChar.toLowerCase()) {
+                if(needleCharIndex == 0) {
+                    var score = (lastWhitespace == hayCharIndex - 1) ? 2 : 1;
+                    result[needleCharIndex].push({positions: [hayCharIndex], score: score});
+                } else {
+                    result[needleCharIndex-1].forEach(function(possibility) {
+                        positions = possibility.positions;
+                        if(positions[positions.length-1] != hayCharIndex) {
+                            var score = possibility.score + 1;
+                            if(positions[positions.length-1] == (hayCharIndex - 1)) {
+                                score += 1;
+                            } else if(lastWhitespace == (hayCharIndex - 1)) {
+                                score += 1;
+                            }
+                            result[needleCharIndex].push({positions: positions.concat(hayCharIndex), score: score});
+                        }
+                    });
+                    // First time match, do not check further needles
+                    if(result[needleCharIndex-1].length == result[needleCharIndex].length) {
+                        return true;
+                    }
+                }
+            } else if(result[needleCharIndex].length == 0) {
+                return true;
+            }
+        });
+    });
+
+    result[needle.length-1].sort(function(a, b) { return b.score - a.score; });
+
+    return result[needle.length-1];
+}
