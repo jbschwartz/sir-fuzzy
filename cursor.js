dee645d1906a6ce1408839c68755af46daa10980
James Schwartz <james@schwartz.engineer>
2015-08-10 21:41:12 -0400

    Switch to implementation using a cursor to iterate through needle and haystack string

---
+++ cursor.js
@@ -0,0 +1,49 @@
+var Cursor = function(string, index) {
+  this.index = index || 0;
+  this.string = string;
+  this.length = string.length;
+  this.character = this.string[this.index];
+  this.is = this;
+}
+
+Cursor.prototype.reset = function(index) {
+  this.index = index || 0;
+  this.character = this.string[this.index];
+}
+
+Cursor.prototype.advance = function(stride) {
+  var stride = stride || 1;
+  this.index += stride;
+  this.character = this.string[this.index];
+  return this.character;
+}
+
+Cursor.prototype.next = function() {
+  return new Cursor(this.string, this.index + 1);
+}
+
+Cursor.prototype.nextCharacter = function() {
+  return this.string[this.index + 1];
+}
+
+Cursor.prototype.firstCharacter = function() {
+  return this.index == 0;
+}
+
+Cursor.prototype.lastCharacter = function() {
+  return this.index == (this.length - 1);
+}
+
+Cursor.prototype.whiteSpace = function() {
+  return /\s/.test(this.character);
+};
+
+Cursor.prototype.punctuation = function () {
+  return /[,.;:]/.test(this.character)
+}
+
+Cursor.prototype.wordBoundary = function() {
+  return this.whiteSpace() || this.punctuation();
+}
+
+module.exports = Cursor;
--- sir-fuzzy.js
+++ sir-fuzzy.js
@@ -1,70 +1,55 @@
-var match = function(needle, haystack) {
+var Cursor = require('./cursor.js');
+
+var match = function(needleString, haystackString) {
     var lastWhitespace = 0;
     var result = [];
-    var is = {
-      Whitespace: function(string) { return /\s/.test(string); },
-      Punctutation: function(string) { return /[,.;:]/.test(string) }
-    }
-    haystack.split("").forEach(function(hayChar, hayCharIndex) {
-        var nextHaystackCharacter = function() {
-          return haystack[hayCharIndex + 1];
-        }
 
-        var isLastHaystackCharacter = function() {
-          return hayCharIndex == (haystack.length - 1);
-        }
+    var haystack = new Cursor(haystackString);
+    var needle = new Cursor(needleString);
 
-        needle.split("").some(function(needleChar, needleCharIndex) {
-            var isLastNeedleChar = function() {
-              return needleCharIndex == (needle.length - 1);
+    do {
+        do {
+            if(!result[needle.index]) { result[needle.index] = [] }
+
+            if(haystack.is.whiteSpace()) {
+                lastWhitespace = haystack.index;
+                continue;
             }
 
-            var isFirstNeedleChar = function() {
-              return needleCharIndex == 0;
-            }
-
-            if(!result[needleCharIndex]) { result[needleCharIndex] = [] }
-
-            if(is.Whitespace(hayChar)) {
-                lastWhitespace = hayCharIndex;
-                return true;
-            }
-
-            if(needleChar.toLowerCase() == hayChar.toLowerCase()) {
-                if(isFirstNeedleChar()) {
-                    var score = (lastWhitespace == hayCharIndex - 1) ? 2 : 1;
-                    result[needleCharIndex].push({positions: [hayCharIndex], score: score});
+            if(needle.character.toLowerCase() == haystack.character.toLowerCase()) {
+                if(needle.is.firstCharacter()) {
+                    var score = (lastWhitespace == haystack.index - 1) ? 2 : 1;
+                    result[needle.index].push({positions: [haystack.index], score: score});
                 } else {
-                    result[needleCharIndex-1].forEach(function(possibility) {
-                        positions = possibility.positions;
-                        if(positions[positions.length-1] != hayCharIndex) {
+                    result[needle.index-1].forEach(function(possibility) {
+                        var positions = possibility.positions;
+                        if(positions[positions.length-1] != haystack.index) {
                             var score = possibility.score + 1;
-                            if(positions[positions.length-1] == (hayCharIndex - 1)) {
+                            if(positions[positions.length-1] == (haystack.index - 1)) {
                                 score += 1;
-                            } else if(lastWhitespace == (hayCharIndex - 1)) {
+                            } else if(lastWhitespace == (haystack.index - 1)) {
                                 score += 1;
                             }
 
-                            if(isLastNeedleChar() && ((hayCharIndex - positions[0]) == (needle.length - 1))) {
-                              if(is.Whitespace(nextHaystackCharacter()) ||
-                                 is.Punctutation(nextHaystackCharacter()) ||
-                                 isLastHaystackCharacter()) {
-                                score += 1.5
+                            if(needle.is.lastCharacter() && ((haystack.index - positions[0]) == (needle.length - 1))) {
+                              if(haystack.next().is.wordBoundary() || haystack.is.lastCharacter()) {
+                                score += 1.5;
                               }
                             }
-                            result[needleCharIndex].push({positions: positions.concat(hayCharIndex), score: score});
+                            result[needle.index].push({positions: positions.concat(haystack.index), score: score});
                         }
                     });
                     // First time match, do not check further needles
-                    if(result[needleCharIndex-1].length == result[needleCharIndex].length) {
-                        return true;
+                    if(result[needle.index-1].length == result[needle.index].length) {
+                        continue;
                     }
                 }
-            } else if(result[needleCharIndex].length == 0) {
-                return true;
+            } else if(result[needle.index].length == 0) {
+                continue;
             }
-        });
-    });
+        } while(needle.advance());
+        needle.reset();
+    } while(haystack.advance());
 
     result[needle.length-1].sort(function(a, b) { return b.score - a.score; });
 
